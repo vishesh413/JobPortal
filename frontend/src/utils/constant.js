b172688ff6f3745892dfe424deebabf191fcdb7e
Vishesh Dwivedi <visheshd223@gmail.com>
2025-07-08 23:50:53 +0530

    add base url for backend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:5000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:5000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:5000/api/v1/company";
+const BASE = import.meta.env.VITE_API_BASE_URL;
+
+export const USER_API_END_POINT = `${BASE}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE}/api/v1/company`;
